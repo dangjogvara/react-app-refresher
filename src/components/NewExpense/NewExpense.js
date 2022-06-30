79a6b3aa85c7d9ada1125dfb1933ac7f76739136
Dan Poulsen <dangjogvara@gmail.com>
2022-06-30 21:04:43 +0100

    add cancel button

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -14,16 +14,22 @@ const NewExpense = props => {
     };
 
     props.onAddExpense(expenseData);
+    setIsEditing(false);
   };
 
   const startEditingHandler = () => {
     setIsEditing(true);
   };
 
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
   return (
       <div className="new-expense">
         {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
-        {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />}
+        {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}
+                                   onCancel={stopEditingHandler} />}
       </div>
   );
 };
