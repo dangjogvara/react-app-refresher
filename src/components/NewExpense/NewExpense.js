7463a9f51d99245b12556e6af619a56ad417dae3
Dan Poulsen <dangjogvara@gmail.com>
2022-06-30 20:56:56 +0100

    conditional editing

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,12 @@
+import { useState } from 'react';
+
 import ExpenseForm from './ExpenseForm';
 
 import './NewExpense.css';
 
 const NewExpense = props => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = enteredExpenseData => {
     const expenseData = {
       ...enteredExpenseData,
@@ -12,10 +16,15 @@ const NewExpense = props => {
     props.onAddExpense(expenseData);
   };
 
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
   return (
-    <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-    </div>
+      <div className="new-expense">
+        {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+        {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />}
+      </div>
   );
 };
 
