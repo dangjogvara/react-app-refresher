50bc649852baa4319c0d37a3d83fbf829da2f0b6
Dan Poulsen <dangjogvara@gmail.com>
2022-06-30 19:51:56 +0100

    filter list by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,17 +17,23 @@ const Expenses = props => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => {
+      return <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          key={expense.id}
+      />;
+    });
+  }
+
   return (
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {filteredExpenses.map((expense) => {
-          return <ExpenseItem
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-              key={expense.id}
-          />;
-        })}
+        {expensesContent}
       </Card>
   );
 };
